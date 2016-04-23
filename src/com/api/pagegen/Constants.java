050212a0f79daa64da00fbf4000852254e3a6260
omaray <omar.develops@gmail.com>
2016-04-23 02:45:12 -0700

    Placing resources in respective folders

--- src/com/api/pagegen/Constants.java
+++ src/com/api/pagegen/Constants.java
@@ -3,10 +3,10 @@ package com.api.pagegen;
 public class Constants {
 
     // Path to json data files
-    public static final String APIS_JSON_PATH = "./resources/apis.json";
-    public static final String CLOUD_JSON_PATH = "./resources/cloud.json";
+    public static final String APIS_JSON_PATH = "./resources/data/apis.json";
+    public static final String CLOUD_JSON_PATH = "./resources/data/cloud.json";
     public static final String APIARY_JSON_PATH = "./resources/apiary.json";
-    public static final String LANGUAGES_JSON_PATH = "./resources/languages.json";
+    public static final String LANGUAGES_JSON_PATH = "./resources/data/languages.json";
     
     // Api related constants for markdown
     public static final String API_DISPLAY_NAME = "apiDisplayName";
@@ -37,10 +37,10 @@ public class Constants {
     public static final String DETAILS_VARIABLES_FILE_NAME = "%s_%s_variables_details_page.md";
     
     // Templates
-    public static final String API_PAGE_TEMPLATE = "./resources/api_details_template.mustache";
-    public static final String API_VARIABLE_PAGE_TEMPLATE = "./resources/api_details_variables_template.mustache";
-    public static final String API_LANDING_TEMPLATE = "./resources/api_landing_template.mustache";
-    public static final String API_LANDING_CLOUD_TEMPLATE = "./resources/api_landing_cloud_template.mustache";
+    public static final String API_PAGE_TEMPLATE = "./resources/templates/api_details_template.mustache";
+    public static final String API_VARIABLE_PAGE_TEMPLATE = "./resources/templates/api_details_variables_template.mustache";
+    public static final String API_LANDING_TEMPLATE = "./resources/templates/api_landing_template.mustache";
+    public static final String API_LANDING_CLOUD_TEMPLATE = "./resources/templates/api_landing_cloud_template.mustache";
     
     // Api Categories
     public static String[] API_CATEGORIES = 
